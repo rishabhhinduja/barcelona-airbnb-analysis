58668ce0ef5b83c9ca95c6037755e5f15c8a75ef
rishabhhinduja <rishabhhinduja745@gmail.com>
2026-07-05 01:47:48 +0530

    feat: Implement high-performance bulk ingestion pipeline
    - Built automated BULK INSERT scripts to seed the data warehouse from cleaned local CSV files.
    - Enforced strict relational execution order (Dimension tables loaded first, followed by Fact tables) to respect database constraints.
    - Optimized for machine execution speed, bypassing standard UI memory limits to seamlessly ingest the 5.8-million-row calendar fact table.

---
+++ 02_bulk_load_warehouse.sql
@@ -0,0 +1,76 @@
+GO
+
+BULK INSERT Dim_Host
+FROM 'D:\Airbnb Project\cleaned_data\listings_dim_host.csv'
+WITH (
+    FORMAT = 'CSV',          
+    FIRSTROW = 2,            
+    FIELDTERMINATOR = ',',   
+    ROWTERMINATOR = '\n'     
+)
+
+GO
+
+BULK INSERT Dim_Neighbour
+FROM 'D:\Airbnb Project\cleaned_data\listings_dim_neighbourhood.csv'
+WITH (
+    FORMAT = 'CSV',
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n'
+)
+
+GO
+
+BULK INSERT Dim_Listings
+FROM 'D:\Airbnb Project\cleaned_data\listings_base_clean.csv'
+WITH (
+    FORMAT = 'CSV',
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n'
+)
+
+GO
+
+BULK INSERT Fact_Availability
+FROM 'D:\Airbnb Project\cleaned_data\listings_fact_availability.csv'
+WITH (
+    FORMAT = 'CSV',
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n'
+)
+
+GO
+
+BULK INSERT Fact_Host
+FROM 'D:\Airbnb Project\cleaned_data\listings_fact_host.csv'
+WITH (
+    FORMAT = 'CSV',
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n'
+)
+
+GO
+
+BULK INSERT Fact_Calendar
+FROM 'D:\Airbnb Project\cleaned_data\calendar_cleaned.csv'
+WITH (
+    FORMAT = 'CSV',
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n'
+)
+
+GO
+
+BULK INSERT Fact_Reviews
+FROM 'D:\Airbnb Project\cleaned_data\fact_reviews_clean.csv'
+WITH (
+    FORMAT = 'CSV',
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n'
+)
